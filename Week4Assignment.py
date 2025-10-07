65b462872d1f59e13063b17763ee3f9285d7d1ee
luciabrown <brownlu@tcd.ie>
2025-10-07 16:10:24 +0100

    Warnings for next assignment to make sure it gets included

---
+++ Week4Assignment.py
@@ -0,0 +1,4 @@
+# In your assignments and projects, unless otherwise stated it
+# is mandatory to present cross-validation analysis to support
+# your choice of hyperparameter values.
+# reasonable baseline too
